8c59d63ccb8cd94309c900d4cbddaf92ddb8770b
FlyingArs <davidyu89tech@gmail.com>
2015-11-15 18:47:52 -0500

    Overhaul

--- routes/users.js
+++
@@ -1,6 +0,0 @@
-/*
- * File Name: users.ejs
- * Author's Name: David Yu 200286902
- * Website Name:donaldrich.heroku.com
- * File Desciption: this is a file created by the system. In this case, we don't need to use it at all. 
- */
