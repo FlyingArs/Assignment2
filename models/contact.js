e060b030ab1056209c3c3e2f4dfbc613d578d6ab
FlyingArs <davidyu89tech@gmail.com>
2015-11-15 19:11:15 -0500

    Add-On

---
+++ models/contact.js
@@ -0,0 +1,27 @@
+﻿//require the moongose object
+var mongoose = require('mongoose');
+
+//create a schema for the the business contact list
+var contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Contact name is required'
+    },
+    number: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Contact number is required'
+    },
+    email: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Email Address is required'
+    }
+});
+
+// make this file public
+module.exports = mongoose.model('Contact', contactSchema);
